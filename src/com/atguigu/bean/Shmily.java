8668d6b3e16f523913fa1ed707afd42279ce161f
like-along <diy19981202@163.com>
2019-12-30 09:16:31 +0800

    不是爱

--- src/com/atguigu/bean/Shmily.java
+++ src/com/atguigu/bean/Shmily.java
@@ -5,6 +5,7 @@ public class Shmily {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("zzzzzz这就是爱吗");
+		System.out.println("zdadasdada不是爱");
 	}
 
 }
